8b36820a4b8fe33f4af901e6248e92c7cf8bf95a
Luis Hernández <luxo997@gmail.com>
2023-08-28 02:41:42 -0500

    Add: delete and Update data EF

--- Program.cs
+++ Program.cs
@@ -33,4 +33,36 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
 
 });
 
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+  var tareaActual = dbContext.Tareas.Find(id);
+
+  if (tareaActual != null) 
+  {
+    tareaActual.CategoriaId = tarea.CategoriaId;
+    tareaActual.Titulo = tarea.Titulo;
+    tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+    tareaActual.Descripcion = tarea.Descripcion;
+    tareaActual.Autor = tarea.Autor;
+
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+  } 
+  return Results.NotFound();
+
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+  var tareaActual = dbContext.Tareas.Find(id);
+  if (tareaActual != null) 
+  {
+    dbContext.Remove(tareaActual);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+  } 
+  return Results.NotFound();
+});
+
 app.Run();
