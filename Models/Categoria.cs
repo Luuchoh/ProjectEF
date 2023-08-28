602725ba06daef26bc6d57c0faa17f33d817d6be
Luis Hernández <luxo997@gmail.com>
2023-08-27 23:57:13 -0500

    Update: nullables

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -8,9 +8,9 @@ public class Categoria
     public Guid CategoriaId { get; set; }
     [Required]
     [MaxLength(150)]
-    public string Nombre { get; set; }
-    
-    public string Descripcion { get; set; }
+    public string? Nombre { get; set; }
 
-    public virtual ICollection<Tarea> Tareas { get; set; }
+    public string? Descripcion { get; set; }
+
+    public virtual ICollection<Tarea>? Tareas { get; set; }
 }
