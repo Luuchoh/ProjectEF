64568c037b359d8d9a648d6244d795915b3a8390
Luis Hernández <luxo997@gmail.com>
2023-08-28 01:45:00 -0500

    Add: add fluent API for annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,13 +4,12 @@ namespace  projectEF.Models;
 
 public class Categoria
 {
-    [Key]
+    //[Key]
     public Guid CategoriaId { get; set; }
-    [Required]
-    [MaxLength(150)]
+    //[Required]
+    //[MaxLength(150)]
     public string? Nombre { get; set; }
-
     public string? Descripcion { get; set; }
-
+    public int Peso { get; set; }
     public virtual ICollection<Tarea>? Tareas { get; set; }
 }
